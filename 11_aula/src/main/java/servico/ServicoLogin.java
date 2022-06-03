f3bf5d9541fe928dd5cfea5971915eb70ddeb632
Técnico Informática 2021.1 <51127512021.1@senacrjedu.local>
2022-06-03 11:30:30 -0300

    Att e Aula 11 serve Experimento

---
+++ 11_aula/src/main/java/servico/ServicoLogin.java
@@ -0,0 +1,12 @@
+package servico;
+
+public class ServicoLogin {
+	public boolean verificarLogin(String usuario, String senha) {
+		if(usuario.equals("admin") && senha.equals("12345")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+}
